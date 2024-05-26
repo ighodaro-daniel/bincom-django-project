5eaccb601f9c51a89be62555ddee5c5c4165b504
ighodaro-daniel <ighodarod95@gmail.com>
2024-05-26 22:16:20 +0100

    modified url

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from  website.views import index, post_details
 
 urlpatterns = [
-    path('',index, name='index'),
+    path('home',index, name='index'),
     path('<slug:slug>/',post_details , name='post_details')
 ]
