547010d76e094c2eb938c0521f3f2b3ff1193b3c
ighodaro-daniel <ighodarod95@gmail.com>
2024-05-26 22:03:42 +0100

    change url

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from  website.views import index, post_details
 
 urlpatterns = [
-    path('home',index, name='index'),
+    path('',index, name='index'),
     path('<slug:slug>/',post_details , name='post_details')
 ]
